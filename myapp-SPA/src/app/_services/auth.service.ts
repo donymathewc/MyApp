5b320c7985184bc3195853bcf0453a660092f9dd
donymathewc <57743294+donymathewc@users.noreply.github.com>
2020-07-03 00:24:56 +0400

    end sec 6

--- myapp-SPA/src/app/_services/auth.service.ts
+++ myapp-SPA/src/app/_services/auth.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 baseurl = 'http://localhost:5000/api/Auth/';
+jwthelper = new JwtHelperService();
+decodedToken: any;
+
 constructor(private http: HttpClient) { }
 login(model: any)
 {
@@ -17,6 +21,7 @@ login(model: any)
       if (user)
       {
         localStorage.setItem('token', user.token);
+        this.decodedToken = this.jwthelper.decodeToken(user.token);
       }
     })
   );
@@ -26,5 +31,9 @@ login(model: any)
 register(model: any)
 {
   return this.http.post(this.baseurl + 'register', model);
+}
+loggedIn(){
+  const token = localStorage.getItem('token');
+  return !this.jwthelper.isTokenExpired(token);
 }
 }
--- myapp-SPA/src/app/app.module.ts
+++ myapp-SPA/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from './register/RegisterComponent';
 import { ErrorInterceptorProvider } from './_services/error.intercepter';
 
 @NgModule({
@@ -19,7 +21,9 @@ import { ErrorInterceptorProvider } from './_services/error.intercepter';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BrowserAnimationsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
